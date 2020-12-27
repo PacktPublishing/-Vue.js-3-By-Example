f9acb7d807da1262d4038f931c19bf0ef1da9135
Ho Hang Au Yeung <mayeung@telus.net>
2020-12-27 14:54:42 -0800

    change test name

--- ch3/tests/unit/puzzles.spec.js
+++ ch3/tests/unit/puzzles.spec.js
@@ -2,13 +2,13 @@ import { mount } from '@vue/test-utils'
 import Puzzles from '@/components/Puzzles.vue'
 
 describe('Puzzles.vue', () => {
-  it('emit puzzled-change event when Play button is clicked', () => {
+  it('emit puzzled-changed event when Play button is clicked', () => {
     const wrapper = mount(Puzzles)
     wrapper.find('.play-button button').trigger('click');
     expect(wrapper.emitted()).toHaveProperty('puzzle-changed');
   })
 
-  it('emit puzzled-change event with the puzzle ID when Play button is clicked', () => {
+  it('emit puzzled-changed event with the puzzle ID when Play button is clicked', () => {
     const wrapper = mount(Puzzles)
     wrapper.find('.play-button button').trigger('click');
     const puzzleChanged = wrapper.emitted('puzzle-changed');
